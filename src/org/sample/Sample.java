ba6dda7bab470ecba7781852644b1ba47fc0a8ec
Vinithkumar1997 <vinithsivakumar14@gmail.com>
2021-01-19 18:55:13 +0530

    1123

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -3,9 +3,7 @@ package org.sample;
 public class Sample {
 	
 	public static void main(String[] args) {
-		System.out.println("Sample1");
-		System.out.println("Sample2");
-		System.out.println("Sample3");
+		
 
 	}
 
