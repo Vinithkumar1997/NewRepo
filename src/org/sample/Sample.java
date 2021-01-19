7cf3a8325a396a482b177802aff0aa770adac2cb
Vinithkumar1997 <vinithsivakumar14@gmail.com>
2021-01-19 19:03:18 +0530

    last

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -3,7 +3,9 @@ package org.sample;
 public class Sample {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Sample 1");
+		System.out.println("Sample 2");
+		System.out.println("Sample 3");
 
 	}
 
